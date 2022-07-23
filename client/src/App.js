43b9440e557c8ed4cdab2fd19793d5c00e51b2d9
prajwalahetti <parjwal.ahetti@gmail.com>
2022-07-23 13:27:29 +0530

    initial commit '

---
+++ client/src/App.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Landing from "./components/layout/Landing";
+import Login from "./components/auth/Login";
+import Logout from "./components/auth/Logout";
+import Dashboard from "./components/dashboard/Dashboard";
+import { Provider } from "react-redux";
+import store from "./store";
+import "./App.css";
+import Alert from "./components/layout/Alert";
+import { useEffect } from "react";
+import { loadUser } from "./actions/auth";
+import PrivateRoute from "./components/routing/PrivateRoute";
+import NavBar from "./components/layout/NavBar";
+const App = () => {
+
+  useEffect(() => {
+   const fetchUser=()=>{
+    store.dispatch(loadUser());
+   }
+   fetchUser();
+  },[]);
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+       
+          <Alert />
+          <NavBar/>
+          <div className="maincontainer">
+          <Routes>
+            
+            <Route path="/" element={<Landing />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/logout" element={<Logout />} />
+            <Route
+              path="/dashboard"
+              element={<PrivateRoute component={Dashboard} />}
+            />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </Provider>
+  );
+};
+
+export default App;
---
+++ client/src/components/auth/Login.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { login } from "../../actions/auth";
+const Login = ({ setAlert, login }) => {
+  const googleAuth = () => {
+    login();
+  };
+
+  return (
+    <div className="container" >
+    <button onClick={googleAuth} className="button">
+      {" "}
+      <div className="logo"></div>
+      <div className="sign-in-text">Sign in with Google</div>
+    </button>
+  </div>
+  );
+};
+login.propTypes = {
+  login: PropTypes.func.isRequired,
+};
+
+export default connect(null, { login })(Login);
